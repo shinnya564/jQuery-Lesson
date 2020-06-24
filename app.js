ece95a58274ce8a6d51f384cff9746467dec7fc3
shinnya <65529573+shinnya564@users.noreply.github.com>
2020-06-24 18:53:51 +0900

    [finish]chapter6

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 
-$(document).ready(function () {
-    $('body').html('<h1>Hello jQuery!!</h1>');
+$(function(){
+    $('.box1').mouseover(function(){
+        $('.box1').addClass('box1-ext');
+    });
+
+    $('.box1').mouseout(function(){
+        $('.box1').removeClass('box1-ext');
+    });
 });
